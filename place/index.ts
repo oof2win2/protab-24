48e98e233e8ddf34036c5ed99915028b664abba2
oof2win2 <honza.koco44@gmail.com>
2024-07-29 11:04:36 +0200

    check if the pixel is already set before changing

--- place/index.ts
+++ place/index.ts
@@ -1,7 +1,7 @@
 import ky from "ky";
 
 const token = "jan-kocourek-98a35af78caa";
-const offset_x = 32;
+const offset_x = 0;
 const offset_y = 0;
 
 type Pixel = {
@@ -14,21 +14,40 @@ type Pixel = {
 };
 const pixels: Pixel[] = [];
 const file = await Bun.file("./output").text();
-for (const line of file.split("\n")) {
+for (const line of file.trim().split("\n")) {
   const [x, y, r, g, b] = line.split(" ").map(Number);
   pixels.push({ x, y, r, g, b });
 }
 
-let i = 0;
+let i = 1;
 for (const pixel of pixels) {
+  const x = pixel.x + offset_x;
+  const y = pixel.y + offset_y;
+  const current = await ky(`http://misto.i.protab.cz/api/get/${x}/${y}`).json<{
+    ok: boolean;
+    color: [number, number, number];
+  }>();
+  if (!current.ok) {
+    console.error("Failed to get current pixel");
+    process.exit();
+  }
+  if (
+    current.color[0] === pixel.r &&
+    current.color[1] === pixel.g &&
+    current.color[2] === pixel.b
+  ) {
+    console.log(`Pixel ${i++}/${pixels.length} is already correct`);
+    continue;
+  }
   const res = await ky("http://misto.i.protab.cz/api/draw", {
     method: "POST",
     json: {
       token,
-      x: pixel.x + offset_x,
-      y: pixel.y + offset_y,
+      x,
+      y,
       color: [pixel.r, pixel.g, pixel.b],
     },
+    throwHttpErrors: false,
   }).json<{
     ok: boolean;
     message: string;
--- place/main.py
+++ place/main.py
@@ -2,13 +2,17 @@ from PIL import Image
 
 TOKEN = "jan-kocourek-98a35af78caa"
 
-im = Image.open("andy.png")
+im = Image.open("gitrebase.png")
 pixels = list(im.getdata())
 width, height = im.size
 pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
 
 for y, row in enumerate(pixels):
     for x, pixel in enumerate(row):
-        r, g, b = pixel
+        if len(pixel) == 3:
+            r, g, b = pixel
+            a = 255
+        else:
+            r, g, b, a = pixel
         if a != 255: continue
-        print(f"{x} {y} {r} {g} {b}")
+        print(f"{x} {y} {r} {g} {b} {a}")
